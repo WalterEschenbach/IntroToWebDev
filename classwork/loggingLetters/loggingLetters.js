47944336fec8229290a6c5c7e27dfd3972b59afc
Walter <eschenbach.walter@gmail.com>
2021-12-01 17:34:52 -0600

    logging letters code along

---
+++ classwork/loggingLetters/loggingLetters.js
@@ -0,0 +1,8 @@
+var sentence = "I love learning software development";
+
+for (let i = 0; i < sentence.length; i++) {
+    if(sentence[i] !== " "){
+        console.log(sentence[i]);
+    }
+    
+}
